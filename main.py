b9fb90b1bad36bc8f39ff816c4adf591b4f18383
Darkkvod <da>
2021-03-12 12:03:40 +0300

    Get and print TLE

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
 import time
 import sgp4
+from get_tle import *
 from skyfield.api import load, Topos, EarthSatellite
 
 TLE_FILE = "https://celestrak.com/NORAD/elements/active.txt"  # DB file to download
-SAT_NAME = "NOAA 19"
+SAT_NAME = "NOAA 19                 "
 # LK coords
 LATITUDE = 55.93013
 LONGITUDE = 37.51832
+
+from_strings(TLE_FILE, SAT_NAME)
